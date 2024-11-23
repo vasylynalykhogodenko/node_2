b5ec6d4e40153d22313aee8333bc89d892fd04d3
Vasylyna Lykhogodenko <devvasylyna@gmail.com>
2024-11-23 19:41:14 +0200

    feat: add url on task03

--- index.js
+++ index.js
@@ -3,58 +3,58 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const handlers = {
+'/sum': sum
+};
+
 const server = http.createServer((req, res) => {
-  if (req.method === 'GET') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ status: 'ok', timestamp: new Date() }));
-  } else if (req.method === 'POST') {
-    parseBodyJson(req, (err, payload) => {
+ parseBodyJson(req, (err, payload) => {
+ const handler = getHandler(req.url);
 
-        if (err) {
-        res.statusCode = 400;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ error: 'Invalid JSON' }));
-        return;
-      }
+ handler(req, res, payload, (err, result) => {
+ if (err) {
+ res.statusCode = err.code;
+ res.setHeader('Content-Type', 'application/json');
+ res.end( JSON.stringify(err) );
 
-      if (typeof payload.a !== 'number' || typeof payload.b !== 'number') {
-        res.statusCode = 400;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ error: 'Payload must contain numbers "a" and "b"' }));
-        return;
-      }
+ return;
+ }
 
-      const c = { c: payload.a + payload.b };
-
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(c));
-    });
-  } else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ error: 'Not Found' }));
-  }
+ res.statusCode = 200;
+ res.setHeader('Content-Type', 'application/json');
+ res.end( JSON.stringify(result) );
+ });
+ });
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+ console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-function parseBodyJson(req, callback) {
-  let body = '';
+function getHandler(url) {
+ return handlers[url] || notFound;
+}
 
-  req.on('data', chunk => {
-    body += chunk;
-  });
+function sum(req, res, payload, cb) {
+ const result = { c: payload.a + payload.b };
 
-  req.on('end', () => {
-    try {
-      const parsed = JSON.parse(body);
-      callback(null, parsed);
-    } catch (error) {
-      callback(error);
-    }
-  });
+ cb(null, result);
+}
+
+function notFound(req, res, payload, cb) {
+ cb({ code: 404, message: 'Not found'});
+}
+
+function parseBodyJson(req, cb) {
+ let body = [];
+
+ req.on('data', function(chunk) {
+ body.push(chunk);
+}).on('end', function() {
+body = Buffer.concat(body).toString();
+
+let params = JSON .parse(body);
+
+cb(null, params);
+});
 }
